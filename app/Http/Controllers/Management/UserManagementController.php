3ae84c039e1218e545994ab881823c01b86e3cb2
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-12-27 20:07:17 -0600

    Set random password to user

--- app/Http/Controllers/Management/UserManagementController.php
+++ app/Http/Controllers/Management/UserManagementController.php
@@ -5,44 +5,49 @@ namespace App\Http\Controllers\Management;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class UserManagementController extends Controller
 {
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return Inertia::render(component: 'Management/CreateUser', props: []);
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		return Inertia::render(component: 'Management/CreateUser', props: []);
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-				$request->validate(rules:[
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request)
+	{
+		$request->validate(rules: [
 
-					'name' => 'required|string|max:255',
-					'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+			'name' => 'required|string|max:255',
+			'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
 
-				]);
+		]);
 
-				User::create([
-					'name' => $request->name,
-					'email' => $request->email
-				]);
+		User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => Hash::make(
+				value: Str::random(24)
+			)
+		]);
 
-				return redirect(to: route(name:'user.list'));
-    }
+		return redirect(to: route(name: 'user.list'));
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(User $user)
+	{
+		//
+	}
 }
