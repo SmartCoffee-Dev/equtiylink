a8cba4df5d43b3835d033896d3a741ae1be96828
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-12-28 13:31:13 -0600

    Define a Super-Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Super Admin',
-            'email' => 'super.equitylink@yopmail.com',
-						'password' => Hash::make(value: 'password')
-        ]);
+				$this->call(class: [
+					PermissionSeeder::class,
+					UserSeeder::class
+				]);
     }
 }
