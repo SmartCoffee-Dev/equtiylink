498b9af27845be18500e635f3be7aeb1e7c409c6
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2024-12-29 17:50:49 -0600

    Provide an endpoint to get the available permissions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PermissionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth')->group(function() {
+
+	Route::get(uri: '/permissions', action: [PermissionController::class, 'index'])->name('api.permissions.index');
 });
