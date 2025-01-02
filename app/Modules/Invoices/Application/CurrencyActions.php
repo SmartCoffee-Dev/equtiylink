0aeb03dc46c0862bca3e9768fbc8d247436e8dbf
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2025-01-02 11:52:21 -0600

    Get rate for DEG currencies from Banxico-SieAPIRest

---
+++ app/Modules/Invoices/Application/CurrencyActions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\Modules\Invoices\Application;
+
+use App\Modules\Invoices\Domain\CurrencyRateEnum;
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class CurrencyActions
+{
+
+	const UNSUPPORTED_CURRENCY_MESSAGE = 'Unsupported currency. Please, try from the series-code';
+	const MISSING_BMX_TOKEN_MESSAGE = 'Missing Bim Token';
+
+	static function currencyRateFromCode(string $code): float
+	{
+
+		$currencyCase = CurrencyRateEnum::tryFrom($code);
+
+		if (!$currencyCase) {
+			throw new Exception(message: self::UNSUPPORTED_CURRENCY_MESSAGE, code: 422);
+		}
+
+		return self::currencyRateFromSerie($currencyCase->name);
+
+	}
+
+	static function currencyRateFromSerie(string $serie): float
+	{
+
+		$bmxToken = env('BANXICO_TOKEN');
+
+		if (!$bmxToken) {
+			throw new Exception(message: self::MISSING_BMX_TOKEN_MESSAGE);
+		}
+
+		$response = Http::withHeaders([
+			'Bmx-Token' => env(key: 'BANXICO_TOKEN')
+		])
+			->acceptJson()
+			->get("https://www.banxico.org.mx/SieAPIRest/service/v1/series/$serie/datos/oportuno");
+
+		$jsonResponse = $response->json();
+
+		return (float) $jsonResponse['bmx']['series'][0]['datos'][0]['dato'];
+	}
+}
---
+++ app/Modules/Invoices/Domain/CurrencyRateEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Modules\Invoices\Domain;
+
+enum CurrencyRateEnum: string
+{
+
+	case SF43718 = 'USD';
+	case SF46410 = 'EUR';
+	case SF46406 = 'JPY';
+	case SF46407 = 'GBP';
+	case SF60632 = 'CAD';
+}
