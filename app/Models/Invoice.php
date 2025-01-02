3752ea95ce855a15ddc2e09767715b808eb3f57d
SmartCoffee-Dev <smartcoffee.dev@gmail.com>
2025-01-01 22:51:44 -0600

    Fix both Invoice model and migration

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -5,12 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @var string $uuid
+ * @var string $folio
+ * @var string $emisor
+ * @var string $receptor
+ * @var string $moneda
+ * @var float $total
+ * @var float $tasa_de_cambio
+ */
 class Invoice extends Model
 {
 	use HasFactory;
 
 	protected $fillable = [
-		'UUID',
+		'uuid',
 		'folio',
 		'emisor',
 		'receptor',
--- database/migrations/2025_01_02_013945_create_invoices_table.php
+++ database/migrations/2025_01_02_013945_create_invoices_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-						$table->uuid();
+						$table->string('uuid');
 						$table->string('folio');
 						$table->string('emisor');
 						$table->string('receptor');
